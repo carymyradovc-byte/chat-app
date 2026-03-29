8d32ca88f7f6ade579eba4404c4be5e4b6dc895d
carymyradovc-byte <carymyradovc@gmail.com>
2026-03-30 00:29:01 +0500

    server.js

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require('express');
-const express = require('express');
 const app = express();
 
-// 🔥 MUHIM: Railway uchun port
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(__dirname));
 
-// xabar qabul qilish
 app.post('/send', (req, res) => {
-    const message = req.body.message;
-    console.log("YANGI XABAR:", message);
+    console.log("YANGI XABAR:", req.body.message);
     res.json({ status: "ok" });
 });
 
-// serverni ishga tushirish
 app.listen(PORT, () => {
-    console.log(`Server ishlayapti: ${PORT}`);
+    console.log("Server ishlayapti:", PORT);
 });
